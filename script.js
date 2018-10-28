8d9d0d51161b67e0c41264170f7ac622491136c7
Pieter Haase <pieter.haase@haw-hamburg.de>
2018-10-28 08:35:33 +0100

    erstes Design

---
+++ script.js
@@ -0,0 +1,5 @@
+var textOutput = document.getElementById("textOutput");
+
+document.getElementById("button").addEventListener("mouseup", function(e){
+    textOutput.innerHTML = "button pressed"
+});

ed7fad75dd9f9d32ef72260f2b7acd5142c83d7c
Pieter Haase <pieter.haase@haw-hamburg.de>
2018-12-05 18:09:07 +0100

    tremolo-delay-update

--- javascript/effects/tremolo.js
+++ javascript/effects/tremolo.js
@@ -1,26 +1,27 @@
 
 function Tremolo(thisContext) {
     this.setEffectType("Tremolo");
-    var frequency = {value: 10};
-    var mix = {value: 40};
+    var rate = {value: 4};
+    var depth = {value: 6};
 
     var gain = thisContext.createGain();
     gain.gain.value = 1;
 
     var dryMix = thisContext.createGain();
-    dryMix.gain.value = 1-mix.value/100;
+    dryMix.gain.value = 1-depth.value/10;
 
     var wetMix = thisContext.createGain();
-    wetMix.gain.value = mix.value/100;
+    wetMix.gain.value = depth.value/10-0.1;
 
     var oscillator = thisContext.createOscillator();
     oscillator.type = "sine";
-    oscillator.frequency.value = frequency.value;
+    oscillator.frequency.value = rate.value*2;
     oscillator.connect(gain.gain);
     oscillator.start(); 
     this.addNode(gain);
     
-    this.addParameter("Mix", "%", 0, 100, mix, 1);
+    this.addParameter("Rate", "", 0, 10, rate, 0.1);
+    this.addParameter("Depth", "", 0, 10, depth, 0.1);
 
     this.getInput().connect(dryMix);
     dryMix.connect(this.getWetGain());
@@ -29,8 +30,9 @@ function Tremolo(thisContext) {
     wetMix.connect(this.getWetGain());
 
     this.updateParameters = function() {
-        dryMix.gain.value = 1-mix.value/100;
-        wetMix.gain.value = mix.value/100;
+        dryMix.gain.value = 1-depth.value/10;
+        wetMix.gain.value = depth.value/10-0.1;
+        oscillator.frequency.value = rate.value*2;
     }
 }
    

c841614f551719b168cc9c75a0e0a6af63633aba
Pieter Haase <pieter.haase@haw-hamburg.de>
2018-12-18 01:04:50 +0100

    Compressor Update

--- javascript/effects/compressor.js
+++ javascript/effects/compressor.js
@@ -2,24 +2,28 @@
 function Compressor() {
 
     this.setEffectType("Compressor");
-    var threshold = {value: -50};
+    var threshold = {value: -60};
     var ratio = {value: 10};
-//    var level = {value: -20};
+    var makeUpGain = {value: 6};
 
     var compressor = context.createDynamicsCompressor();
     compressor.threshold.value = threshold.value;
     compressor.ratio.value = ratio.value;
 
+    var gain = context.createGain();
+    gain.gain.value = 1 + makeUpGain.value / 5;
     
     this.addParameter("Threshold", "dB", -100, 0, threshold, 1);
     this.addParameter("Ratio", "", 1, 20, ratio, 0.1);
-    //this.addParameter("Level", "dB", -40, 40, level, 1);
+    this.addParameter("Gain", "", 0, 10, makeUpGain, 0.1);
 
     this.getInput().connect(compressor);
-    compressor.connect(this.getWetGain());
+    compressor.connect(gain);
+    gain.connect(this.getWetGain());
 
     this.updateParameters = function() {
         compressor.threshold.value = threshold.value;
         compressor.ratio.value = ratio.value;
+        gain.gain.value = 1 + makeUpGain.value / 5;
     }
 }
